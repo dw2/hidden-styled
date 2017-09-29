1d1662a91ccf921aa04f76782cab6e762a6ef5b6
Douglas Waltman <dwaltman@weedmaps.com>
2017-09-29 14:29:57 -0700

    Adds .0001 to min-width ranges to prevent breakpoint overlaps

--- src/Hide.js
+++ src/Hide.js
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 
 export const breakpoints = {
   xs: '@media screen and (max-width: 40em)',
-  sm: '@media screen and (min-width: 40em) and (max-width: 52em)',
-  md: '@media screen and (min-width: 52em) and (max-width: 64em)',
-  lg: '@media screen and (min-width: 64em)',
+  sm: '@media screen and (min-width: 40.0001em) and (max-width: 52em)',
+  md: '@media screen and (min-width: 52.0001em) and (max-width: 64em)',
+  lg: '@media screen and (min-width: 64.0001em)',
 }
 
 export const hidden = key => props => props[key] ? {
